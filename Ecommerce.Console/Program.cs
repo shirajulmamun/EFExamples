733771adda48140099feec81e1198d59f875336c
shirajulmamun <shirajul.mamun@gmail.com>
2018-01-20 15:13:43 +0600

    DbContext Connected Object

--- Ecommerce.Console/Program.cs
+++ Ecommerce.Console/Program.cs
@@ -13,17 +13,20 @@ namespace Ecommerce.Console
         static void Main(string[] args)
         {
             EcommerceDbContext db = new EcommerceDbContext();
+            var product = db.Products.FirstOrDefault(c => c.Id == 4);
 
-            // SELECT Name,Code FROM Products product WHERE product.Name LIKE '%h%'
+            product.CategoryId = 1;
 
-            var retriveProducts = db.Products
-                            .Where(c => c.Name.ToLower().Contains("h"))
-                            .Select(c=>new { Name=c.Name,Code=c.Code});
-
-            foreach(var p in retriveProducts)
+            bool isUpdated = db.SaveChanges() > 0;
+            if(isUpdated)
             {
-                System.Console.WriteLine("Name: " + p.Name + " Code: " + p.Code);
+                System.Console.WriteLine("Updated Successfully!");
             }
+            else
+            {
+                System.Console.WriteLine("Update Failed!");
+            }
+          
 
             System.Console.ReadKey();
 
