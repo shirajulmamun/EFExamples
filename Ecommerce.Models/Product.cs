bceea382c970854ffdd802408a57435eaf63fe45
sakhawatliv <sakhawatliv@gmail.com>
2018-01-20 12:38:30 +0600

    Example Project Deleted

---
+++ Ecommerce.Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; } 
+        public int ParenId { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Ecommerce.Repository/EcomerceDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Repository
+{
+    public class EcomerceDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> productCategories { get; set; }
+    }
+}
