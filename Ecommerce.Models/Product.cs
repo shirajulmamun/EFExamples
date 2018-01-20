9ab02f95084d52a63004e80e1edc8fa653768041
Md.Raihan <hriddo13@gmail.com>
2018-01-20 12:12:35 +0600

    Product, productCategory class added

---
+++ Ecommerce.Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Models
+{
+    class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Code { get; set; }
+
+        public int? ParentId { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+
+
+    }
+}
---
+++ Ecommerce.Models/ProductCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Models
+{
+    class ProductCategory
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        
+    }
+}
